70f2221cacccde8de97a704cfaaab402f38a1000
Vladislav Koroteev <koroteev.v.n@mail.ru>
2025-01-14 15:57:09 +0300

    Создан класс клиентов

--- Test_console_chat/Clients.h
+++ Test_console_chat/Clients.h
@@ -1,3 +1,13 @@
 #pragma once
 #include<iostream>
+#include<string>
 
+class Client
+{
+public:
+	std::string _username;
+	std::string _email;
+	std::string _password;
+
+	Client(std::string username, std::string email, std::string password):_username(username),_email(email),_password(password){}
+};
